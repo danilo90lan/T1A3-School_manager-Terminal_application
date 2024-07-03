b9140e0a50a631554616bb7fa6415dfb8d562666
danilo90lan <15630@coderacademy.edu.au>
2024-07-03 23:34:55 +1000

    added comments to the class Person, Teacher, Student

--- src/models/student.py
+++ src/models/student.py
@@ -1,18 +1,32 @@
 from models import Person
 
 class Student(Person):
+    """
+    The Student class is a sub class of Person class and includes additional attributes
+    and methods specific to students
+    """
     # class const
     profile = "Student"
+
     def __init__(self, name, last_name, address, course):
+        """ The constructor initializes a new instance of Student 
+        It calls the constructor of the Person class using super()
+        to initialize the inherited attributes and then 
+        initializes the course attribute with the provided parameter
+        """
         super().__init__(name, last_name, address)
         self.course = course
         self.profile = Student.profile
         self.__id = Person.get_id()
 
     def get_id(self):
+        """ This getter method returns the ID of the student instance"""
         return self.__id
 
     def print_info(self):
+        """
+        return: This method returns a string with the both inherited and student information like the course name. 
+        """
         info = f"""
         Profile: {self.profile}
         Course name: {self.course}
@@ -20,5 +34,6 @@ class Student(Person):
         return super().print_info() + info
     
     def update_student(self):
+        """ This method updatates both inherited attributes and the course attribute """
         super().update_info()
         self.course = input("New course --> ").strip().capitalize()
--- src/models/teacher.py
+++ src/models/teacher.py
@@ -1,19 +1,32 @@
 from models import Person
 
 class Teacher(Person):
+    """
+    The Teacher class is a sub class of Person class and includes additional attributes
+    and methods specific to teachers
+    """
     # class const
     profile = "Teacher"
 
     def __init__(self, name, last_name, address, subject_area):
+        """ The constructor initializes a new instance of Teacher
+        It calls the constructor of the Person class using super()
+        to initialize the inherited attributes and then 
+        initializes the subject_area attribute with the provided parameter
+        """
         super().__init__(name, last_name, address)
         self.subject_area = subject_area
         self.profile = Teacher.profile
         self.__id = Person.get_id()
 
     def get_id(self):
+        """ This getter method returns the ID of the teacher instance"""
         return self.__id
 
     def print_info(self):
+        """
+        return: This method returns a string with the both inherited and teacher information like the subject name. 
+        """
         info = f"""
         Profile: {self.profile}
         Teaching subject: {self.subject_area}
@@ -21,5 +34,6 @@ class Teacher(Person):
         return super().print_info() + info
     
     def update_teacher(self):
+            """ This method updatates both inherited attributes and the subject_area attribute """
             super().update_info()
             self.subject_area = input("New teaching subject --> ").strip().capitalize()
